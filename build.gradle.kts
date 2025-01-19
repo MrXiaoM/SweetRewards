cd1a7ae1aa01aad8f8013323b98981f9ba412a18
人間工作 <coolxiaom95@gmail.com>
2025-01-20 01:13:25 +0800

    upgrade PluginBase to 1.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("net.kyori:adventure-api:4.17.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.4")
     implementation("net.kyori:adventure-text-minimessage:4.17.0")
-    implementation("top.mrxiaom:PluginBase:1.2.1")
+    implementation("top.mrxiaom:PluginBase:1.2.2")
 }
 java {
     val javaVersion = JavaVersion.toVersion(targetJavaVersion)
