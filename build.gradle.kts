fc0221fd13e09d374b4e210776266d3382c99287
MrXiaoM <coolxiaom95@gmail.com>
2025-06-26 20:59:12 +0800

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,15 +24,15 @@ dependencies {
 
     compileOnly("me.clip:placeholderapi:2.11.6")
 
-    implementation("de.tr7zw:item-nbt-api:2.15.0")
+    implementation("de.tr7zw:item-nbt-api:2.15.1-SNAPSHOT")
     implementation("com.zaxxer:HikariCP:4.0.3")
     implementation("org.slf4j:slf4j-nop:2.0.16")
     implementation("org.jetbrains:annotations:24.0.0")
-    implementation("net.kyori:adventure-api:4.21.0")
+    implementation("net.kyori:adventure-api:4.22.0")
     implementation("net.kyori:adventure-platform-bukkit:4.4.0")
-    implementation("net.kyori:adventure-text-minimessage:4.21.0")
+    implementation("net.kyori:adventure-text-minimessage:4.22.0")
     implementation("com.github.technicallycoded:FoliaLib:0.4.4")
-    implementation("top.mrxiaom:PluginBase:1.4.4")
+    implementation("top.mrxiaom:PluginBase:1.4.9")
 }
 java {
     val javaVersion = JavaVersion.toVersion(targetJavaVersion)
